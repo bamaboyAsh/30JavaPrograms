04823d0346fe2c8c72da39c6273289394ba7ab2e
Ashutosh Salunke <a.ashu002@gmail.com>
2018-11-19 16:59:37 +0530

    Commit
    Commit

---
+++ src/PatternPrograms/JavaPyramid2.java
@@ -0,0 +1,27 @@
+package PatternPrograms;
+
+public class JavaPyramid2 
+{
+
+	public static void main(String[] args) 
+	{
+		for(int i=5; i>0; i--)
+		{
+			for (int j=0; j<i; j++)
+			{
+				System.out.print(j+1);
+			}
+			System.out.println("");
+		}
+	}
+
+}
+//
+
+//Output of the example would be
+//12345
+//1234
+//123
+//12
+//1
+
