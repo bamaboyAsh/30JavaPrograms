c4ba33bfbce3ac8cb7f492939e42b32172e46c1f
Ashutosh Salunke <a.ashu002@gmail.com>
2018-11-29 14:25:18 +0530

    commit
    palindrome program

---
+++ src/coding/inteview/programs/PalidromeOrNot.java
@@ -0,0 +1,43 @@
+package coding.inteview.programs;
+
+import java.util.Scanner;
+
+public class PalidromeOrNot 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a string to check if palindrome or not");
+		//taking input from user.
+		String input= sc.nextLine();
+		
+		//Convert String to String buffer
+		
+		StringBuffer sb= new StringBuffer();
+		StringBuffer reverse= new StringBuffer();
+		
+		
+		sb.append(input);
+		System.out.println("Input given by user= "+input);
+		System.out.println("String converted to StringBuffer= "+sb+". Both are same");
+		
+		//Now reversing the string using built in Method reverse.
+		
+		reverse= sb.reverse();
+		String rev =reverse.toString();
+		
+		System.out.println(sb);
+		
+		//A string is said to be Palindrome if and only if the reverse of the string is equal to original string
+		
+		if(rev.equals(input))
+		{
+			System.out.println("Input String "+ sb +"  is Palindrome");
+		}
+		else
+			System.out.println("String is not plaindrome");
+		
+	}
+
+}
