abf4ab6ac66938b0efd0b2354c44149f3fff4681
Ashutosh Salunke <a.ashu002@gmail.com>
2018-12-12 04:27:40 +0530

    Numbers1
    Write a Java program that accepts three integers from the user and return true if the second number is greater than first number and third number is greater than second number. If "abc" is true second number does not need to be greater than first number.

---
+++ src/coding/inteview/programs/Numbers1.java
@@ -0,0 +1,14 @@
+package coding.inteview.programs;
+/*
+ Write a Java program that accepts three integers from the user and return true if the second number 
+ is greater than first number and third number is greater than second number. 
+ If "abc" is true second number does not need to be greater than first number.
+ */
+public class Numbers1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
