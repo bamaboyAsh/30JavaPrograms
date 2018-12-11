6c1767f99a417f422d6c10b6240bca57b58c25af
Ashutosh Salunke <a.ashu002@gmail.com>
2018-12-12 03:20:19 +0530

    Printing ascii value of char

---
+++ src/coding/inteview/programs/PrintASCIIValueOfChar.java
@@ -0,0 +1,22 @@
+package coding.inteview.programs;
+
+//  Write a Java program to print the ascii value of a given character.
+public class PrintASCIIValueOfChar {
+
+	public static void main(String[] args) {
+		
+		//logic is to save the char in integer variable then it will print its ascii value
+		
+		int a ='S';
+		int b= 'z';
+		int c= 'J';
+		int d= 'H';
+		int e= 'o';
+		
+		System.out.println(a+" "+b+" "+c+" "+d+" "+e);
+		
+	
+	}
+
+}
+//output is: 83 122 74 72 111
