a7bf2f77ba5fb080eec25fff27d9295afdd9d881
Ashutosh Salunke <a.ashu002@gmail.com>
2018-12-12 20:19:21 +0530

    sum of first 100 prime numbers

---
+++ src/coding/inteview/programs/SumOfFirst100PrimeNumbers.java
@@ -0,0 +1,43 @@
+package coding.inteview.programs;
+
+import java.util.Scanner;
+
+//Write a Java program to compute the sum of the first 100 prime numbers.
+public class SumOfFirst100PrimeNumbers {
+
+	public static void main(String[] args) 
+	{
+		int sum = 1;
+		int ctr = 0; 
+		int n = 0; 
+	    	
+		while (ctr < 100) 
+		{
+		n++; 
+			if (n % 2 != 0) 
+			{ 
+				if (is_Prime(n)) 
+				{
+					sum += n; 
+					ctr++; 
+					}
+				}	
+			}
+			System.out.println("\nSum of the first 100 prime numbers: "+sum); 	
+		 }
+		
+	   	public static boolean is_Prime(int n) 
+	   	{
+			for (int i = 3; i * i <= n; i+= 2) 
+			{
+				if (n % i == 0) 
+				{
+					return false; 
+				}
+			}
+			return true;
+		}
+		
+	
+
+}
