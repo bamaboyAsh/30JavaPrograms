7eb3ffcf49ce434c2a155323af79cbb91c22541b
Ashutosh Salunke <a.ashu002@gmail.com>
2018-12-12 21:09:24 +0530

    String
    copy half of the first even string

---
+++ src/com/w3package/Copyfirst_half_of_evenString.java
@@ -0,0 +1,13 @@
+package com.w3package;
+
+public class Copyfirst_half_of_evenString {
+
+	public static void main(String[] args) {
+		String str= "Python";
+		
+		
+		System.out.println(str.substring(0, str.length()/2));
+
+	}
+
+}
